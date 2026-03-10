a27cef585a52b7885229f7584b1c156ee3e0bc67
Diegogonzalezm2022 <diegogonzalezm2022@gmail.com>
2026-03-10 12:40:46 +0000

    Refactored insert_question.js to use querySelector.

--- src/javascript/insert_question.js
+++ src/javascript/insert_question.js
@@ -6,12 +6,14 @@ async function insert_question(test_id, question_number) {
                 if (test.test_id === test_id) {
                     test.questions.forEach((question) => {
                         if (question.question_id === question_number) {
-                            document.getElementsByClassName("question-text")[0].innerHTML = question.Question_title;
+                            document.querySelector('h2[class="question-text"]').innerHTML = question.Question_title;
                             for (let i=0; i<question.Options.length; i++) {
-                                document.getElementById("ans"+(i+1)+"_label").innerHTML=question.Options[i].Option_name
+                                label_id= "ans" + (i+1) + "_label";
+                                document.querySelector('label[id='+label_id+']').innerHTML=question.Options[i].Option_name;
                             }
                             for (let i=question.Options.length; i<4;i++) {
-                                document.getElementById("option"+(i+1)).style.display="none";
+                                option_id= "option" + (i+1);
+                                document.querySelector('div[id='+option_id+']').style.display="none";
 
                             }
                         }
