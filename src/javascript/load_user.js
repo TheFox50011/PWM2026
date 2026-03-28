c78401820816273f175b643bbf88d68ca317e33a
TheFox50011 <saulpabblofg2@gmail.com>
2026-03-28 11:14:01 +0000

    Minor fixes on editprofile and created register.js

--- src/javascript/load_user.js
+++ src/javascript/load_user.js
@@ -4,6 +4,7 @@ async function loadUserProfile(passedId) {
         const urlId = urlParams.get('id');
         const loggedId = localStorage.getItem('loggedUserId');
         const userId = urlId || passedId || loggedId || 1;
+
         const response = await fetch('../assets/users.json');
         const defaultUsers = await response.json();
         const localUsers = JSON.parse(localStorage.getItem('myRegisteredUsers')) || [];
@@ -32,11 +33,9 @@ async function loadUserProfile(passedId) {
         }
 
         if (user) {
-            const savedChanges = localStorage.getItem('userProfileChanges');
-            const searchAvatar = document.getElementById('search-avatar');
-            if (savedChanges) {
-                const parsedChanges = JSON.parse(savedChanges);
-                user = { ...user, ...parsedChanges };
+            const allSavedChanges = JSON.parse(localStorage.getItem('allUserProfileChanges')) || {};
+            if (allSavedChanges[userId]) {
+                user = { ...user, ...allSavedChanges[userId] };
             }
 
             if(document.getElementById('profile-name')) document.getElementById('profile-name').textContent = user.username;
@@ -45,13 +44,11 @@ async function loadUserProfile(passedId) {
             if(document.getElementById('profile-following')) document.getElementById('profile-following').textContent = user.Following;
             if(document.getElementById('profile-bio')) document.getElementById('profile-bio').textContent = user.Biography;
 
-
             if (user.Profile_picture) {
                 const fotoPath = user.Profile_picture.includes('/') ? user.Profile_picture : `../assets/${user.Profile_picture}`;
                 if(document.getElementById('profile-picture')) document.getElementById('profile-picture').src = fotoPath;
                 if(document.getElementById('profile-picture-small')) document.getElementById('profile-picture-small').src = fotoPath;
             }
-
 
             if (user.Student_person) {
                 if(document.getElementById('study-person')) document.getElementById('study-person').src = user.Student_person;
@@ -66,7 +63,6 @@ async function loadUserProfile(passedId) {
                     window.location.href = `EditProfile.html`;
                 };
             }
-
         } else {
             console.warn("Usuario no encontrado.");
         }
