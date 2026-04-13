e8f515ed9d1f743faea155344fe838a39a78a0cf
catthyy <catthyy25@gmail.com>
2026-04-13 20:18:39 +0100

    forgot-your-password.html, forgot-your-password.css and routes added.

--- Angular/src/app/forgot-your-password/forgot-your-password.ts
+++ Angular/src/app/forgot-your-password/forgot-your-password.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from '../components/header/header';
+import {FooterComponent} from '../components/footer/footer';
 
 @Component({
   selector: 'app-forgot-your-password',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './forgot-your-password.html',
   styleUrl: './forgot-your-password.css',
 })
