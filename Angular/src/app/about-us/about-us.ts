98a578afc209cc8a92fcf1b2ad5e9a648fd9d053
Diegogonzalezm2022 <diegogonzalezm2022@gmail.com>
2026-04-13 20:18:46 +0100

    Added about us page to angular.

--- Angular/src/app/about-us/about-us.ts
+++ Angular/src/app/about-us/about-us.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from '../components/header/header';
+import {FooterComponent} from '../components/footer/footer';
 
 @Component({
   selector: 'app-about-us',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './about-us.html',
   styleUrl: './about-us.css',
 })
-export class AboutUs {}
+export class AboutUs {
+  image1="background4.PNG"
+  image2="logo.png"
+}
--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -3,11 +3,12 @@ import {IndexComponent} from './index';
 import {Login} from './login/login';
 import {CreateAnAccount} from './create-an-account/create-an-account';
 import {OurTeam} from './our-team/our-team';
+import {AboutUs} from './about-us/about-us';
 
 export const routes: Routes = [
   {path: "", component: IndexComponent},
   {path: "login", component: Login},
   {path: "create-an-account", component: CreateAnAccount},
   {path: "our-team", component: OurTeam},
-
+  {path: "about-us", component: AboutUs},
 ];
