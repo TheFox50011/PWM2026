f164ca11835d6dc292a7ee472f0b7b7d03b817ac
Diegogonzalezm2022 <diegogonzalezm2022@gmail.com>
2026-04-13 19:15:55 +0100

    Added our team page to Angular.

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import {IndexComponent} from './index';
+import {OurTeam} from './our-team/our-team';
 
 export const routes: Routes = [
   {path: "", component: IndexComponent},
+  {path: "our-team", component: OurTeam},
 
 ];
--- Angular/src/app/our-team/our-team.ts
+++ Angular/src/app/our-team/our-team.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import {FooterComponent} from '../components/footer/footer';
+import {HeaderComponent} from '../components/header/header';
 
 @Component({
   selector: 'app-our-team',
-  imports: [],
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './our-team.html',
   styleUrl: './our-team.css',
 })
-export class OurTeam {}
+export class OurTeam {
+
+}
