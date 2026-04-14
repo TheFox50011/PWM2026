ba19379f0171c258b870a5f37c158fb43eb5de19
Diegogonzalezm2022 <diegogonzalezm2022@gmail.com>
2026-04-14 15:50:46 +0100

    Fixed buttons in mainpage not working.

--- Angular/src/app/mainpage/mainpage.ts
+++ Angular/src/app/mainpage/mainpage.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {FooterComponent} from '../components/footer/footer';
 import {HeaderComponent} from '../components/header/header';
 import {AsideComponent} from '../components/sidebar/sidebar';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-mainpage',
   imports: [
     FooterComponent,
     HeaderComponent,
-    AsideComponent
+    AsideComponent,
+    RouterLink
   ],
   templateUrl: './mainpage.html',
   styleUrl: './mainpage.css',
