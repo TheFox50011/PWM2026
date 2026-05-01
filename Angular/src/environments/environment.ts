56a7b7434b15e5b0aa2842f4a09271d834bea284
catthyy <catthyy25@gmail.com>
2026-05-01 13:29:01 +0100

    Some styles added.

---
+++ Angular/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAEOC_Ev9Hb8dR9XC4-jGcuBWLIQPUn64Q",
+    authDomain: "studyhub-eeba0.firebaseapp.com",
+    projectId: "studyhub-eeba0",
+    storageBucket: "studyhub-eeba0.firebasestorage.app",
+    messagingSenderId: "892238547716",
+    appId: "1:892238547716:web:9f7918d76b8ef0e02d5e51"
+  }
+};
