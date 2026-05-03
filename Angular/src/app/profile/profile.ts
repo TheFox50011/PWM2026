66e401ce3baaa8e351272ab57a1a178f32d61b25
catthyy <catthyy25@gmail.com>
2026-05-03 15:35:13 +0100

    Forums and tests created appears in profile section.

--- Angular/src/app/profile/profile.ts
+++ Angular/src/app/profile/profile.ts
@@ -4,7 +4,7 @@ import { FooterComponent } from '../components/footer/footer';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Auth, authState } from '@angular/fire/auth';
-import { Firestore, doc, onSnapshot } from '@angular/fire/firestore';
+import { Firestore, doc, onSnapshot, collection, query, where, getDocs, orderBy } from '@angular/fire/firestore';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -31,16 +31,12 @@ export class Profile implements OnInit, OnDestroy {
   link2: string = '';
   sharedFiles: any[] = [];
   sharedTests: any[] = [];
+  sharedForums: any[] = [];
 
   ngOnInit(): void {
     this.authSub = authState(this.auth).subscribe(user => {
       this.unsubSnapshot?.();
       if (user) {
-        // Filtra solo los tests creados por este usuario
-        const savedTests = localStorage.getItem('availableTests');
-        const allTests = savedTests ? JSON.parse(savedTests) : [];
-        this.sharedTests = allTests.filter((t: any) => t.author_id === user.uid);
-
         const userRef = doc(this.firestore, `users/${user.uid}`);
         this.unsubSnapshot = onSnapshot(userRef, snapshot => {
           const data = snapshot.data();
@@ -57,11 +53,36 @@ export class Profile implements OnInit, OnDestroy {
           }
           this.cdr.detectChanges();
         });
+
+        this.loadUserTests(user.uid);
+        this.loadUserForums(user.uid);
       } else {
         this.userName = 'No has iniciado sesión';
         this.cdr.detectChanges();
       }
     });
+  }
+
+
+
+  async loadUserForums(uid: string) {
+    const q = query(
+      collection(this.firestore, 'forums'),
+      where('author_id', '==', uid)
+    );
+    const snap = await getDocs(q);
+    this.sharedForums = snap.docs.map(d => ({ id: d.id, ...d.data() }));
+    this.cdr.detectChanges();
+  }
+
+  async loadUserTests(uid: string) {
+    const q = query(
+      collection(this.firestore, 'tests'),
+      where('author_id', '==', uid)
+    );
+    const snap = await getDocs(q);
+    this.sharedTests = snap.docs.map(d => ({ id: d.id, ...d.data() }));
+    this.cdr.detectChanges();
   }
 
   ngOnDestroy(): void {
