e4823ec6b1222fd7708b0bd049e71ba190942a9d
Diegogonzalezm2022 <diegogonzalezm2022@gmail.com>
2026-05-11 19:31:47 +0100

    Added ionic package.

--- Angular/src/app/app.config.ts
+++ Angular/src/app/app.config.ts
@@ -10,6 +10,7 @@ import { provideFirebaseApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
+import { provideIonicAngular } from '@ionic/angular/standalone';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,7 +18,7 @@ export const appConfig: ApplicationConfig = {
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore()),
     provideAuth(() => getAuth()),
-    provideStorage(() => getStorage())
+    provideStorage(() => getStorage()), provideIonicAngular({})
 
   ]
 };
