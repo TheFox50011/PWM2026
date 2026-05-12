18444fd1cbaca87b8999638d3dcab66fd952464f
Diegogonzalezm2022 <diegogonzalezm2022@gmail.com>
2026-05-12 15:38:17 +0100

    Added project configurations.

---
+++ Angular/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.Studyhub',
+  appName: 'Studyhub',
+  webDir: 'src'
+};
+
+export default config;
