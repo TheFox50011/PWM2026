e074d104de0f9c8bdb64bcd04b6cf9d6d329b3b6
catthyy <catthyy25@gmail.com>
2026-05-24 15:53:13 +0100

    Some errors fixed.

--- Angular/src/app/app.ts
+++ Angular/src/app/app.ts
@@ -1,9 +1,11 @@
 import {Component, signal} from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { AsideComponent } from './components/sidebar/sidebar';
+
 
 @Component({
   selector: 'app-root',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, AsideComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
